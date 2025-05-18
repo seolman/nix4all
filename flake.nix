789d1506a36a970967fe8dd12c78ffc4e134d519
seolman <tjfehdgns@gmail.com>
2025-05-18 18:54:19 +0900

    .

--- flake.nix
+++ flake.nix
@@ -1,56 +1,69 @@
 {
-	description = "nix for all";
+  description = "nix for all";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-		home-manager = {
-			url = "github:nix-community/home-manager";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		nur = {
-			url = "github:nix-community/NUR";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		nixos-wsl = {
-			url = "github:nix-community/NixOS-WSL";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		darwin = {
-			url = "github:nix-darwin/nix-darwin";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		stylix = {
-			url = "github:danth/stylix";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-	};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    darwin = {
+      url = "github:nix-darwin/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  };
 
-	outputs = { self, nixpkgs, home-manager, nur, nixos-wsl, darwin, stylix, ... }@inputs: let
-		system = "x86_64-linux";
-    packages = nixpkgs.legacyPackages.${system};
-	in {
-		nixosConfigurations = {
-			nixoslaptop = nixpkgs.lib.nixosSystem {
-				inherit system;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      nur,
+      nixos-wsl,
+      darwin,
+      stylix,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+      packages = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations = {
+        nixoslaptop = nixpkgs.lib.nixosSystem {
+          inherit system;
           specialArgs = { inherit inputs; };
-				modules = [
-					home-manager.nixosModules.home-manager
-					nur.modules.nixos.default
-					stylix.nixosModules.stylix
-					./hosts/laptop/configuration.nix
-				];
-			};	
-			nixoswsl = nixpkgs.lib.nixosSystem {
-				system = "x86_64-linux";
-				modules = [
-					home-manager.nixosModules.home-manager
-					nur.modules.nixos.default
-					stylix.nixosModules.stylix
-					nixos-wsl.nixosModules.default
-					./hosts/wsl/configuration.nix
-				];
-			};	
-		};
-	};
+          modules = [
+            home-manager.nixosModules.home-manager
+            nur.modules.nixos.default
+            stylix.nixosModules.stylix
+            ./hosts/laptop/configuration.nix
+          ];
+        };
+        nixoswsl = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            home-manager.nixosModules.home-manager
+            nur.modules.nixos.default
+            stylix.nixosModules.stylix
+            nixos-wsl.nixosModules.default
+            ./hosts/wsl/configuration.nix
+          ];
+        };
+      };
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+    };
 }
--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,166 +1,187 @@
-{ config, pkgs, inputs, ... }: {
-	imports = [
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  imports = [
     ./hardware-configuration.nix
     inputs.home-manager.nixosModules.home-manager
-	];
+  ];
 
-	boot.loader.systemd-boot.enable = true;
-	boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-	networking.hostName = "nixoslaptop";
-	networking.networkmanager.enable = true;
+  networking.hostName = "nixoslaptop";
+  networking.networkmanager.enable = true;
 
-	time.timeZone = "Asia/Seoul";
+  time.timeZone = "Asia/Seoul";
 
-	i18n.defaultLocale = "en_US.UTF-8";
-	i18n.extraLocaleSettings = {
-		LC_ADDRESS = "en_US.UTF-8";
-		LC_IDENTIFICATION = "en_US.UTF-8";
-		LC_MEASUREMENT = "en_US.UTF-8";
-		LC_MONETARY = "en_US.UTF-8";
-		LC_NAME = "en_US.UTF-8";
-		LC_NUMERIC = "en_US.UTF-8";
-		LC_PAPER = "en_US.UTF-8";
-		LC_TELEPHONE = "en_US.UTF-8";
-		LC_TIME = "en_US.UTF-8";
-	};
-	i18n.inputMethod = {
-		enable = true;
-		type = "kime";
-		kime = {
-			iconColor = "White";
-			daemonModules = [ "Xim" "Wayland" ];
-			extraConfig = ''
-engine:
-  global_hotkeys:
-    S-Space:
-      behavior: !Toggle
-      - Hangul
-      - Latin
-      result: Consume
-    Esc:
-      behavior: !Switch Latin
-      result: Bypass'';
-		};
-		# type = "fcitx5";
-		# fcitx5.addons = with pkgs; [
-		# 	fcitx5-mozc
-		# 	fcitx5-gtk
-		# 	fcitx5-hangul
-		# ];
-	};
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+  i18n.inputMethod = {
+    enable = true;
+    type = "kime";
+    kime = {
+      iconColor = "White";
+      daemonModules = [
+        "Xim"
+        "Wayland"
+      ];
+      extraConfig = ''
+        engine:
+          global_hotkeys:
+            S-Space:
+              behavior: !Toggle
+              - Hangul
+              - Latin
+              result: Consume
+            Esc:
+              behavior: !Switch Latin
+              result: Bypass'';
+    };
+    # type = "fcitx5";
+    # fcitx5.addons = with pkgs; [
+    # 	fcitx5-mozc
+    # 	fcitx5-gtk
+    # 	fcitx5-hangul
+    # ];
+  };
 
-	powerManagement = {
-		enable = true;
-		powertop.enable = true;
-	};
-	services.auto-cpufreq.enable = true;
-	services.auto-cpufreq.settings = {
-	  battery = {
-	     governor = "powersave";
-	     turbo = "never";
-	  };
-	  charger = {
-	     governor = "performance";
-	     turbo = "auto";
-	  };
-	};
-	services.upower.enable = true;
+  powerManagement = {
+    enable = true;
+    powertop.enable = true;
+  };
+  # hardware.fancontrol = {
+  #   enable = true;
+  #   config = '''';
+  # };
+  # programs.coolercontrol.enable = true;
+  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
+    battery = {
+      governor = "powersave";
+      turbo = "never";
+    };
+    charger = {
+      governor = "performance";
+      turbo = "never";
+    };
+  };
+  services.thermald = {
+    enable = true;
+    ignoreCpuidCheck = true;
+  };
+  services.upower.enable = true;
 
-	services.printing = {
-		enable = true;
-		drivers = with pkgs; [ gutenprint hplip splix ];
-		cups-pdf.enable = true;
-	};
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [
+      gutenprint
+      hplip
+      splix
+    ];
+    cups-pdf.enable = true;
+  };
 
-	hardware.bluetooth = {
-		enable = true;
-		powerOnBoot = true;
-		settings = { General = { Experimental = true; }; };
-	};
-	services.blueman.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+      };
+    };
+  };
+  services.blueman.enable = true;
 
-	security.rtkit.enable = true;
-	services.pipewire = {
-	  enable = true;
-		audio.enable = true;
-	  alsa.enable = true;
-	  alsa.support32Bit = true;
-	  pulse.enable = true;
-		jack.enable = true;
-		wireplumber.enable = true;
-	};
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    audio.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+  };
 
-	security.polkit.enable = true;
-	security.soteria.enable = true;
+  security.polkit.enable = true;
+  security.soteria.enable = true;
 
-	fonts = {
-		enableDefaultPackages = false;
-		packages = with pkgs; [
-			noto-fonts
-			noto-fonts-cjk-sans
-			noto-fonts-cjk-serif
-			noto-fonts-emoji
-			nerd-fonts.jetbrains-mono
-			nanum
-		];
-		fontconfig = {
-			enable = true;
-			defaultFonts.serif = [ "NanumSquareRound Bold" ];
-			defaultFonts.sansSerif = [ "NanumSquareRound Bold" ];
-			defaultFonts.monospace = [ "JetBrainsMono Nerd Font" ];
-			defaultFonts.emoji = [ "Noto Color Emoji" ];
-		};
-	};
+  fonts = {
+    enableDefaultPackages = false;
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-emoji
+      nerd-fonts.jetbrains-mono
+      nanum
+    ];
+    fontconfig = {
+      enable = true;
+      defaultFonts.serif = [ "NanumSquareRound Bold" ];
+      defaultFonts.sansSerif = [ "NanumSquareRound Bold" ];
+      defaultFonts.monospace = [ "JetBrainsMono Nerd Font" ];
+      defaultFonts.emoji = [ "Noto Color Emoji" ];
+    };
+  };
 
-	services.openssh = {
-		enable = true;
-		settings.PasswordAuthentication = false;
-	};
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+  };
 
-	services.tailscale.enable = true;
+  services.tailscale.enable = true;
 
-	# programs.uwsm = {
-	# 	enable = true;
-	# 	waylandCompositors = {};
-	# };
-
-	# services.desktopManager.cosmic.enable = true;
-	# services.displayManager.cosmic-greeter.enable = true;
+  # services.desktopManager.cosmic.enable = true;
+  # services.displayManager.cosmic-greeter.enable = true;
 
   # services.xserver.enable = true;
   # services.displayManager.sddm.enable = true;
   # services.displayManager.sddm.wayland.enable = true;
   # services.desktopManager.plasma6.enable = true;
 
-	# services.xserver.enable = true;
-	# services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
 
-	services.kanata = {
-		enable = true;
-		keyboards."main" = {
-			config = ''
-				(defsrc
-					caps
-					lctl lmet lalt)
-				(defalias
-					cap (tap-hold 100 100 esc lctl))
-				(deflayer main
-					lctl
-					caps lalt lmet)'';
-			devices = [
-				"dev/input/by-path/platform-i8042-serio-0-event-kbd"
-			];
-		};
-	};
+  services.kanata = {
+    enable = true;
+    keyboards."main" = {
+      config = ''
+        				(defsrc
+        					caps
+        					lctl lmet lalt)
+        				(defalias
+        					cap (tap-hold 100 100 esc lctl))
+        				(deflayer main
+        					lctl
+        					caps lalt lmet)'';
+      devices = [
+        "dev/input/by-path/platform-i8042-serio-0-event-kbd"
+      ];
+    };
+  };
 
-	services.mpd = {
-		enable = true;
-		# musicDirectory = "~/Music";
-	};
+  services.mpd = {
+    enable = true;
+    # musicDirectory = "~/Music";
+  };
 
-	services.flatpak.enable = true;
-	systemd.services.flatpak-repo = {
+  services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
     wantedBy = [ "multi-user.target" ];
     path = [ pkgs.flatpak ];
     script = ''
@@ -168,316 +189,319 @@ engine:
     '';
   };
 
-	services.syncthing = {
-		enable = true;
-		user = "seolman";
-		group = "users";
-		dataDir = "/home/seolman/Documents";
-		configDir = "/home/seolman/.config/syncthing";
-	};
+  services.syncthing = {
+    enable = true;
+    user = "seolman";
+    group = "users";
+    dataDir = "/home/seolman/Documents";
+    configDir = "/home/seolman/.config/syncthing";
+  };
 
-	virtualisation.libvirtd = {
-		enable = true;
-		nss.enable = true;
-		qemu.vhostUserPackages = with pkgs; [ virtiofsd ];
-		qemu.swtpm.enable = true;
-	};
-	programs.virt-manager.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    nss.enable = true;
+    qemu.vhostUserPackages = with pkgs; [ virtiofsd ];
+    qemu.swtpm.enable = true;
+  };
+  programs.virt-manager.enable = true;
 
-	virtualisation.docker.enable = true;
+  virtualisation.docker.enable = true;
 
-	# virtualisation.waydroid.enable = true;
+  virtualisation.waydroid.enable = true;
 
-	xdg.portal = {
-		enable = true;
-		config.common.default = "*";
-		wlr.enable = true;
-		xdgOpenUsePortal = true;
-		extraPortals = with pkgs; [
-			xdg-desktop-portal-gtk
-			xdg-desktop-portal-gnome
-		];
-	};
-	services.gnome.gnome-keyring.enable = true;
+  xdg.portal = {
+    enable = true;
+    config.common.default = "*";
+    wlr.enable = true;
+    xdgOpenUsePortal = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
+    ];
+  };
+  services.gnome.gnome-keyring.enable = true;
 
-	xdg.terminal-exec.enable = true;
+  xdg.terminal-exec.enable = true;
 
-	xdg.mime.defaultApplications = {};
+  xdg.mime.defaultApplications = { };
 
-	users.users.seolman = {
-		isNormalUser = true;
-		description = "seolman";
-		# shell = pkgs.zsh;
-		extraGroups = [ "networkmanager" "wheel" "docker" "libvirtd" ];
-		packages = with pkgs; [];
-	};
+  users.users.seolman = {
+    isNormalUser = true;
+    description = "seolman";
+    # shell = pkgs.zsh;
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "docker"
+      "libvirtd"
+    ];
+    packages = with pkgs; [ ];
+  };
 
-    home-manager = {
-      extraSpecialArgs = { inherit inputs; };
-      users = {
-        "seolman" = import ./home.nix;
-      };
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "seolman" = import ./home.nix;
     };
+  };
 
-	nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 
-	programs.niri.enable = true;
+  programs.niri.enable = true;
 
-	# boot.plymouth = {
-	# 	enable = true;
-	# 	theme = "nixos-bgrt";
-	# 	themePackages = with pkgs; [
-	# 		nixos-bgrt-plymouth
-	# 	];
-	# };
+  # boot.plymouth = {
+  # 	enable = true;
+  # 	theme = "nixos-bgrt";
+  # 	themePackages = with pkgs; [
+  # 		nixos-bgrt-plymouth
+  # 	];
+  # };
 
   services.greetd = {
-		enable = true;
-		settings = {
-			default_session = {
-				command = "niri-session";
-				user = "seolman";
-			};
-		};
-		greeterManagesPlymouth = true;
-	};
+    enable = true;
+    settings = {
+      default_session = {
+        command = "niri-session";
+        user = "seolman";
+      };
+    };
+    greeterManagesPlymouth = true;
+  };
 
-	# programs.hyprland.enable = true;
+  # programs.hyprland.enable = true;
 
-	# programs.waybar.enable = true;
+  # programs.waybar.enable = true;
 
-	hardware.graphics = {
-		enable = true;
-		enable32Bit = true;
-	};
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
 
-	services.xserver.videoDrivers = [ "amdgpu" ];
+  services.xserver.videoDrivers = [ "amdgpu" ];
 
-	programs.steam = {
-		enable = true;
-		gamescopeSession.enable = true;
-	};
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
 
-	programs.gamemode.enable = true;
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = true;
+  };
 
-	qt = {
-		enable = true;
-		style = "adwaita-dark";
-		platformTheme = "qt5ct";
-	};
+  programs.gamemode.enable = true;
 
-	programs.starship = {
-		enable = true;
-		presets = [ "plain-text-symbols" ];
-	};
+  qt = {
+    enable = true;
+    style = "adwaita-dark";
+    platformTheme = "qt5ct";
+  };
 
-	programs.fzf = {
-		fuzzyCompletion = true;
-		keybindings = true;
-	};
+  programs.starship = {
+    enable = true;
+    presets = [ "plain-text-symbols" ];
+  };
 
-	programs.zoxide = {
-		enable = true;
-	};
+  programs.fzf = {
+    fuzzyCompletion = true;
+    keybindings = true;
+  };
 
-	programs.zsh = {
-		enable = true;
-		histSize = 10000;
-		enableBashCompletion = true;
-		syntaxHighlighting.enable = true;
-		autosuggestions.enable = true;
-	};
+  programs.zoxide = {
+    enable = true;
+  };
 
-	programs.tmux = {
-		enable = true;
-		plugins = with pkgs; [
-			tmuxPlugins.resurrect
-			tmuxPlugins.continuum
-		];
-		terminal = "screen-256color";
-		shortcut = "g";
-		historyLimit = 10000;
-		escapeTime = 0;
-		baseIndex = 1;
-		# TODO vi mode, split pane and new tab current dir
-		extraConfig = ''
-			set -g mode-keys vi
+  programs.zsh = {
+    enable = true;
+    histSize = 10000;
+    enableBashCompletion = true;
+    syntaxHighlighting.enable = true;
+    autosuggestions.enable = true;
+  };
 
-			run-shell ${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect/resurrect.tmux
-			run-shell ${pkgs.tmuxPlugins.continuum}/share/tmux-plugins/continuum/continuum.tmux
+  programs.tmux = {
+    enable = true;
+    plugins = with pkgs; [ ];
+    terminal = "screen-256color";
+    shortcut = "g";
+    historyLimit = 10000;
+    escapeTime = 0;
+    baseIndex = 1;
+    keyMode = "vi";
+    customPaneNavigationAndResize = true;
+    extraConfig = ''
+    '';
+  };
 
-			set -g @continuum-restore 'on'
-		'';
-	};
+  environment.systemPackages = with pkgs; [
+    git
+    tmux
+    neovim
+    zellij
+    helix
+    yazi
+    ffmpeg
+    p7zip
+    jq
+    poppler
+    fd
+    ripgrep
+    zoxide
+    resvg
+    imagemagick
+    ghostscript
+    wl-clipboard
+    clapboard
+    gitoxide
+    gitu
+    gitui
+    lazygit
+    github-cli
+    rsync
+    fzf
+    skim
+    bat
+    bottom
+    translate-shell
+    fastfetch
+    xh
+    macchina
+    nushell
+    tokei
 
-	environment.systemPackages = with pkgs; [
-		git
-		tmux
-		neovim
-		zellij
-		helix
-		yazi
-		ffmpeg
-		p7zip
-		jq
-		poppler
-		fd
-		ripgrep
-		zoxide
-		resvg
-		imagemagick
-		ghostscript
-		wl-clipboard
-		clapboard
-		gitoxide
-		gitu
-		gitui
-		lazygit
-		github-cli
-		rsync
-		fzf
-		skim
-		bat
-		bottom
-		translate-shell
-		fastfetch
-		xh
-		macchina
-		nushell
+    gcc
+    gnumake
+    gdb
+    gtest
+    clang
+    lldb
+    cmake
+    valgrind
+    mono
+    temurin-bin
+    go
+    rustc
+    python3Minimal
+    nodejs
+    typescript
+    lua
+    luajit
+    typst
+    ansible
+    sqlite
 
-		gcc
-		gnumake
-		gdb
-		gtest
-		clang
-		lldb
-		cmake
-		valgrind
-		mono
-		temurin-bin
-		go
-		rustc
-		python3Minimal
-		nodejs
-		typescript
-		lua
-		luajit
-		typst
-		ansible
-		sqlite
+    nixd
+    clang-tools
+    python313Packages.python-lsp-server
+    lua-language-server
+    typescript-language-server
+    gopls
+    rust-analyzer
+    jdt-language-server
+    bash-language-server
+    marksman
+    tinymist
+    sqls
+    ansible-language-server
+    vscode-langservers-extracted
+    superhtml
+    yaml-language-server
+    taplo
 
-		nixd
-		clang-tools
-		python313Packages.python-lsp-server
-		lua-language-server
-		typescript-language-server
-		gopls
-		rust-analyzer
-		jdt-language-server
-		bash-language-server
-		marksman
-		tinymist
-		sqls
-		ansible-language-server
-		vscode-langservers-extracted
-		superhtml
-		yaml-language-server
-		taplo
+    google-chrome
+    firefox
+    wezterm
+    kitty
+    ghostty
+    neovide
+    vscode
+    zed-editor
+    emacs
+    # zathura
+    # sioyek
+    fuzzel
+    # anyrun
+    nemo-with-extensions
+    nwg-look
+    obsidian
+    obs-studio # screen capture not working
+    obs-studio-plugins.wlrobs
+    # showmethekey
+    # vesktop
+    brightnessctl
+    libreoffice-qt6
+    moonlight-qt
+    gimp3
+    blender
+    # grimblast
+    # hyprpicker
+    cava
+    trashy
+    localsend
+    # udiskie
+    mpv
+    # pass
+    gnupg
+    swww
+    # mako
+    # swaynotificationcenter
+    # swayosd
+    networkmanagerapplet
+    xwayland-satellite
+    mangohud
+    protonup-qt
+    lutris
+    ironbar
+    sxhkd
+    wev
+    font-manager
+    pwvucontrol
+    wayland-utils
+    inori
+    tailscale-systray
+    syncthingtray
+    gtklock
+    wf-recorder
+    shared-mime-info
+    kalker
+    qalculate-gtk
+    shotcut
+    # kmod
+    # fanctl
+    # fan2go
 
-		google-chrome
-		firefox
-		wezterm
-		kitty
-		ghostty
-		alacritty
-		rio
-		warp-terminal
-		neovide
-		# vscode
-		# zed-editor
-		# emacs
-		# zathura
-		# sioyek
-		fuzzel
-		# anyrun
-		nemo-with-extensions
-		# imv
-		grimblast
-		nwg-look
-		obsidian
-		obs-studio # screen capture not working
-		obs-studio-plugins.wlrobs
-		showmethekey
-		vesktop
-		brightnessctl
-		libreoffice-qt6
-		moonlight-qt
-		gimp3
-		blender
-		# grimblast
-		# hyprpicker
-		cava
-		trashy
-		localsend
-		udiskie
-		mpv
-		# pass
-		gnupg
-		swww
-		# mako
-		# swaynotificationcenter
-		# swayosd
-		networkmanagerapplet
-		xwayland-satellite
-		mangohud
-		protonup-qt
-		lutris
-		ironbar
-		sxhkd
-		wev
-		font-manager
-		pwvucontrol
-		wayland-utils
-		inori
-		tailscale-systray
-		syncthingtray
-		gtklock
-		wf-recorder
-		shared-mime-info
-		squeekboard
-		rink
-		kalker
-		qalculate-gtk
+    adw-gtk3
+    adwaita-icon-theme
+    whitesur-gtk-theme
+    whitesur-icon-theme
+    apple-cursor
+  ];
 
-		adw-gtk3
-		adwaita-icon-theme
-		whitesur-gtk-theme
-		whitesur-icon-theme
-		apple-cursor
-	];
-
-	environment.variables = {
-		EDITOR = "hx";
-		VISUAL = "neovide";
-		NIXOS_OZONE_WL = "1";
-		QT_QPA_PLATFORM = "wayland";
-		_ZO_DOCTOR = "0";
-		GOPATH = "~/.go";
-		FZF_DEFAULT_OPTS="
+  environment.variables = {
+    EDITOR = "hx";
+    VISUAL = "neovide";
+    NIXOS_OZONE_WL = "1";
+    QT_QPA_PLATFORM = "wayland";
+    _ZO_DOCTOR = "0";
+    GOPATH = "~/.go";
+    FZF_DEFAULT_OPTS = "
    		--color=fg:#878580,bg:#100F0F,hl:#CECDC3
    		--color=fg+:#878580,bg+:#1C1B1A,hl+:#CECDC3
    		--color=border:#AF3029,header:#CECDC3,gutter:#100F0F
    		--color=spinner:#24837B,info:#24837B,separator:#1C1B1A
    		--color=pointer:#AD8301,marker:#AF3029,prompt:#AD8301";
-	};
+  };
 
-	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
-	nix.gc = {
-		automatic = true;
-		dates = "weekly";
-		options = "--delete-older-than 7d";
-	};
-	
-	system.stateVersion = "24.11";
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
+  system.stateVersion = "24.11";
 }
